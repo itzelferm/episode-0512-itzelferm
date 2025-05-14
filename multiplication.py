3f1ab9b96c44199df39fb901d92af10beac54cd9
Itzel <itzelfrmna@gmail.com>
2025-05-13 17:43:10 -0700

    Added comments to multiplication file

---
+++ multiplication.py
@@ -0,0 +1,20 @@
+def multiply(factors: list[float]) -> float:
+    product = 1
+    for factor in factors:
+        product = product * factor
+
+    return product
+
+def describe_multiply() -> str:
+    return 'Returns the product of all of the numbers passed to it.'
+
+
+if __name__ == '__main__':
+    import sys
+
+    print(describe_multiply())
+# Convert command-line arguments to float and multiply
+    if len(sys.argv) > 1:
+        factors = [float(arg) for arg in sys.argv[1:]]
+        print(multiply(factors))
+        
